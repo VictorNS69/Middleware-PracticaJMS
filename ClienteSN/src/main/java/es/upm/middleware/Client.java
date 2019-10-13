a312618bf8d34e9ff5797d927308838fb208e9f5
Victor Nieves Sanchez <victor.nieves.sanchez@alumnos.upm.es>
2019-10-13 13:36:41 +0200

    encapsuladas algunas funciones y añadido ID único

--- ClienteSN/src/main/java/es/upm/middleware/Client.java
+++ ClienteSN/src/main/java/es/upm/middleware/Client.java
@@ -1,7 +1,8 @@
 package es.upm.middleware;
 
 import javax.jms.*;
-
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 public class Client {
 	public static void main(String[] args) {
@@ -9,40 +10,34 @@ public class Client {
 
         try {
             ConnectionFactory myConnFactory;
-            Queue usuarios;
-            
-        	// Conexion
+            // Cola de usuarios
+ 			Queue usuarios = crearCola("usuarios");
+ 	        // Conexion
             myConnFactory = new com.sun.messaging.ConnectionFactory();
             Connection myConn = myConnFactory.createConnection();
             
             // Sesion
             Session mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            // Cola de usuarios
-            usuarios = new com.sun.messaging.Queue("usuarios");
-            // Productor
-            MessageProducer myMsgProducer = mySess.createProducer(usuarios);
-
+            
+            // Nombre unico
+            String qname = uniqueName();
             // Enviamos mensaje
-            TextMessage myTextMsg = mySess.createTextMessage();
-            myTextMsg.setText("Soy el usuario 1");
-            System.out.println("Sending Message: " + myTextMsg.getText() + " Queue:" + usuarios.getQueueName());
-            myMsgProducer.send(myTextMsg);
+            enviarMensaje(mySess, usuarios, qname);
+            
 			// Cola Cliente-Servidor
-			Queue colaCS = crearCola("CS");
+			Queue colaCS = crearCola("CS" + qname);
 			// Cola Servidor-Cliente
-			Queue colaSC = crearCola("SC");
+			Queue colaSC = crearCola("SC" + qname);
             // Bucle de espera
  			while(true){
- 				System.out.println("Waiting for Message... ");
- 				// Consumidor
- 				MessageConsumer myMsgConsumer = mySess.createConsumer(colaSC);
- 				myConn.start();
- 				Message msg = myMsgConsumer.receive();
+				Message msg = recibirMensaje(mySess, colaSC, myConn);
+
  				// si recibimos mensaje
  				if (msg instanceof TextMessage) {
  					TextMessage txtMsg = (TextMessage) msg;
- 					System.out.println("Read Message: " + txtMsg.getText() + " Queue:" + colaSC.getQueueName());
-		            myTextMsg.setText("Recibido!");
+ 					System.out.println("Read Message: " + txtMsg.getText() + "\tfrom queue: " + colaSC.getQueueName());
+		            TextMessage myTextMsg = mySess.createTextMessage();
+					myTextMsg.setText("Recibido!");
 
  					break;
  				}
@@ -61,5 +56,24 @@ public class Client {
 	private static Queue crearCola(String name) throws JMSException {
 		return new com.sun.messaging.Queue(name);
 	}
-
+	private static void enviarMensaje(Session mySess, Queue cola, String msg) throws JMSException {
+		// Productor
+		MessageProducer myMsgProducer = mySess.createProducer(cola);
+		// Enviamos mensaje
+        TextMessage myTextMsg = mySess.createTextMessage();
+        myTextMsg.setText(msg);
+        System.out.println("Sending Message: " + myTextMsg.getText() + "\tto queue: " + cola.getQueueName());
+        myMsgProducer.send(myTextMsg);
+	}
+	private static Message recibirMensaje(Session mySess, Queue cola, Connection myConn) throws JMSException {
+		System.out.println("Waiting for Message... ");
+		// Consumidor
+		MessageConsumer myMsgConsumer = mySess.createConsumer(cola);
+		myConn.start();
+		return myMsgConsumer.receive();
+	}
+	private static String uniqueName() {
+		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+		return bean.getName().split("@")[1] + bean.getName().replace("@", "");
+	}
 }
--- ServidorNoticias/src/main/java/es/upm/middleware/Server.java
+++ ServidorNoticias/src/main/java/es/upm/middleware/Server.java
@@ -1,5 +1,8 @@
 package es.upm.middleware;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 import javax.jms.*;
 
 public class Server {
@@ -7,41 +10,30 @@ public class Server {
 		System.out.println("EL SERVIDOR ");
 		try {
 			ConnectionFactory myConnFactory;
-			Queue usuarios;
-			
+            
+			// Cola de usuarios
+			Queue usuarios = crearCola("usuarios");
         	// Conexion
 			myConnFactory = new com.sun.messaging.ConnectionFactory();
 			Connection myConn = myConnFactory.createConnection();
             // Sesion
 			Session mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            // Cola de usuarios
-			usuarios = new com.sun.messaging.Queue("usuarios");
 
 			// Bucle de espera
 			while(true){
-				System.out.println("Waiting for Message... ");
-				// Consumidor
-				MessageConsumer myMsgConsumer = mySess.createConsumer(usuarios);
-				myConn.start();
-				Message msg = myMsgConsumer.receive();
-				// si recibimos mensaje
+				// recibir mensaje
+				Message msg = recibirMensaje(mySess, usuarios, myConn);
 				if (msg instanceof TextMessage) {
 					TextMessage txtMsg = (TextMessage) msg;
-					System.out.println("Read Message: " + txtMsg.getText() + " Queue:" + usuarios.getQueueName());
+					System.out.println("Read Message: " + txtMsg.getText() + "\tfrom queue: " + usuarios.getQueueName());
 					
 					// Cola Cliente-Servidor
-					Queue colaCS = crearCola("CS");
+					Queue colaCS = crearCola("CS" + txtMsg.getText());
 					// Cola Servidor-Cliente
-					Queue colaSC = crearCola("SC");
+					Queue colaSC = crearCola("SC" + txtMsg.getText());
 					
-					// Productor
-					MessageProducer myMsgProducer = mySess.createProducer(colaSC);
-
-		            // Enviamos mensaje
-		            TextMessage myTextMsg = mySess.createTextMessage();
-		            myTextMsg.setText("Recibido!");
-		            System.out.println("Sending Message: " + myTextMsg.getText() + " Queue:" + colaSC.getQueueName());
-		            myMsgProducer.send(myTextMsg);
+					// Enviar mensaje
+					enviarMensaje(mySess, colaSC, "Recibido!");
 					break;
 				}
 			}
@@ -58,5 +50,26 @@ public class Server {
 	}
 	private static Queue crearCola(String name) throws JMSException {
 		return new com.sun.messaging.Queue(name);
+	}
+	private static void enviarMensaje(Session mySess, Queue cola, String msg) throws JMSException {
+		// Productor
+		MessageProducer myMsgProducer = mySess.createProducer(cola);
+		// Enviamos mensaje
+        TextMessage myTextMsg = mySess.createTextMessage();
+        myTextMsg.setText(msg);
+        System.out.println("Sending Message: " + myTextMsg.getText() + "\tto queue: " + cola.getQueueName());
+        myMsgProducer.send(myTextMsg);
+	}
+	
+	private static Message recibirMensaje(Session mySess, Queue cola, Connection myConn) throws JMSException {
+		System.out.println("Waiting for Message... ");
+		// Consumidor
+		MessageConsumer myMsgConsumer = mySess.createConsumer(cola);
+		myConn.start();
+		return myMsgConsumer.receive();
+	}
+	private static String uniqueName() {
+		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+		return bean.getName().split("@")[1] + bean.getName().replace("@", "");
 	}
 }
