3ad001c208db59b1eca53328e5915064feaba774
Victor Nieves Sanchez <victor.nieves.sanchez@alumnos.upm.es>
2019-10-13 12:46:14 +0200

    ahora se tienen 2 colas por cada usuario

--- ClienteSN/src/main/java/es/upm/middleware/Client.java
+++ ClienteSN/src/main/java/es/upm/middleware/Client.java
@@ -5,29 +5,51 @@ import javax.jms.*;
 
 public class Client {
 	public static void main(String[] args) {
+		System.out.println("EL CLIENTE ");
 
         try {
-
             ConnectionFactory myConnFactory;
             Queue usuarios;
-
-
+            
+        	// Conexion
             myConnFactory = new com.sun.messaging.ConnectionFactory();
-
             Connection myConn = myConnFactory.createConnection();
-
+            
+            // Sesion
             Session mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
+            // Cola de usuarios
             usuarios = new com.sun.messaging.Queue("usuarios");
-
+            // Productor
             MessageProducer myMsgProducer = mySess.createProducer(usuarios);
 
+            // Enviamos mensaje
             TextMessage myTextMsg = mySess.createTextMessage();
-            myTextMsg.setText("Ey! soy un usuario");
-            System.out.println("Sending Message: " + myTextMsg.getText());
+            myTextMsg.setText("Soy el usuario 1");
+            System.out.println("Sending Message: " + myTextMsg.getText() + " Queue:" + usuarios.getQueueName());
             myMsgProducer.send(myTextMsg);
+			// Cola Cliente-Servidor
+			Queue colaCS = crearCola("CS");
+			// Cola Servidor-Cliente
+			Queue colaSC = crearCola("SC");
+            // Bucle de espera
+ 			while(true){
+ 				System.out.println("Waiting for Message... ");
+ 				// Consumidor
+ 				MessageConsumer myMsgConsumer = mySess.createConsumer(colaSC);
+ 				myConn.start();
+ 				Message msg = myMsgConsumer.receive();
+ 				// si recibimos mensaje
+ 				if (msg instanceof TextMessage) {
+ 					TextMessage txtMsg = (TextMessage) msg;
+ 					System.out.println("Read Message: " + txtMsg.getText() + " Queue:" + colaSC.getQueueName());
+		            myTextMsg.setText("Recibido!");
 
-            System.out.println("FIN!!!!!!!!!!!! client");
+ 					break;
+ 				}
+ 			}
+
+            System.out.println("FIN!");
+            // Cerramos sesion y conexion
             mySess.close();
             myConn.close();
 
@@ -36,4 +58,8 @@ public class Client {
             jmse.printStackTrace();
         }
     }
+	private static Queue crearCola(String name) throws JMSException {
+		return new com.sun.messaging.Queue(name);
+	}
+
 }
--- ServidorNoticias/src/main/java/es/upm/middleware/Server.java
+++ ServidorNoticias/src/main/java/es/upm/middleware/Server.java
@@ -4,41 +4,59 @@ import javax.jms.*;
 
 public class Server {
 	public static void main( String[] args ){
-		                System.out.println("EL MAIIIIN! ");
+		System.out.println("EL SERVIDOR ");
 		try {
+			ConnectionFactory myConnFactory;
+			Queue usuarios;
+			
+        	// Conexion
+			myConnFactory = new com.sun.messaging.ConnectionFactory();
+			Connection myConn = myConnFactory.createConnection();
+            // Sesion
+			Session mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // Cola de usuarios
+			usuarios = new com.sun.messaging.Queue("usuarios");
 
-            ConnectionFactory myConnFactory;
-            Queue usuarios;
+			// Bucle de espera
+			while(true){
+				System.out.println("Waiting for Message... ");
+				// Consumidor
+				MessageConsumer myMsgConsumer = mySess.createConsumer(usuarios);
+				myConn.start();
+				Message msg = myMsgConsumer.receive();
+				// si recibimos mensaje
+				if (msg instanceof TextMessage) {
+					TextMessage txtMsg = (TextMessage) msg;
+					System.out.println("Read Message: " + txtMsg.getText() + " Queue:" + usuarios.getQueueName());
+					
+					// Cola Cliente-Servidor
+					Queue colaCS = crearCola("CS");
+					// Cola Servidor-Cliente
+					Queue colaSC = crearCola("SC");
+					
+					// Productor
+					MessageProducer myMsgProducer = mySess.createProducer(colaSC);
 
-            myConnFactory = new com.sun.messaging.ConnectionFactory();
+		            // Enviamos mensaje
+		            TextMessage myTextMsg = mySess.createTextMessage();
+		            myTextMsg.setText("Recibido!");
+		            System.out.println("Sending Message: " + myTextMsg.getText() + " Queue:" + colaSC.getQueueName());
+		            myMsgProducer.send(myTextMsg);
+					break;
+				}
+			}
+			System.out.println("FIN!");
+            // Cerramos sesion y conexion
+			mySess.close();
+			myConn.close();
 
-            Connection myConn = myConnFactory.createConnection();
+		} catch (Exception jmse) {
+			System.out.println("Exception occurred : " + jmse.toString());
+			jmse.printStackTrace();
+		}
 
-            Session mySess = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            usuarios = new com.sun.messaging.Queue("usuarios");
-
-
-            while(true){
-                System.out.println("Waiting for Message... ");
-                MessageConsumer myMsgConsumer = mySess.createConsumer(usuarios);
-                myConn.start();
-                Message msg = myMsgConsumer.receive();
-
-                if (msg instanceof TextMessage) {
-                    TextMessage txtMsg = (TextMessage) msg;
-                    System.out.println("Read Message: " + txtMsg.getText());
-                    break;
-                }
-            }
-            System.out.println("FIN!!!!!!!!!!!! server");
-            mySess.close();
-            myConn.close();
-
-        } catch (Exception jmse) {
-            System.out.println("Exception occurred : " + jmse.toString());
-            jmse.printStackTrace();
-        }
-		
+	}
+	private static Queue crearCola(String name) throws JMSException {
+		return new com.sun.messaging.Queue(name);
 	}
 }
