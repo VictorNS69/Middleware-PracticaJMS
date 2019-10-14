955d8481d7529ffb44ff954a5383f4c2237c6c9b
Víctor Nieves Sánchez <victor.nieves.sanchez@alumnos.upm.es>
2019-10-14 15:35:52 +0200

    añadidos scripts para ejecutar cliente y servidor

---
+++ run_client.sh
@@ -0,0 +1,13 @@
+# cd Client directory
+cd ClienteSN
+
+# Clean
+mvn clean
+
+# Package
+
+mvn package
+
+# Run
+java -jar target/ClienteSN-1.0-SNAPSHOT-jar-with-dependencies.jar
+
---
+++ run_server.sh
@@ -0,0 +1,12 @@
+# cd Server directory
+cd ServidorNoticias
+
+# Clean
+mvn clean
+
+# Package
+
+mvn package
+
+# Run
+java -jar target/ServidorNoticias-1.0-SNAPSHOT-jar-with-dependencies.jar
