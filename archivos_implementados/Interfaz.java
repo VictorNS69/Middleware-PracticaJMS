6d7b7949f2f66db469a4a374276046942f8b0694
Miguel Gonzalo Anton <m.ganton@alumnos.upm.es>
2019-10-15 18:12:28 +0100

    Empezando el esqueleto

---
+++ archivos_implementados/Interfaz.java
@@ -0,0 +1,22 @@
+public class Interfaz
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Servicio de Noticias. Login");
+	System.out.println("Nombre de Usuario");
+        switch(str)
+        {
+            case "uno":
+                System.out.println("uno");
+                break;
+            case "dos":
+                System.out.println("dos");
+                break;
+            case "tres":
+                System.out.println("tres");
+                break;
+            default:
+                System.out.println("no coincide");
+        }
+    }
+}
